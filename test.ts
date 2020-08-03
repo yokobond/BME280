96f423a1803a6e858ec7cc2eee18b2647a9fbcaf
yokobond <koji.yokokawa@gmail.com>
2020-08-03 17:48:30 +0900

    Fix to work test.

--- test.ts
+++ test.ts
@@ -2,9 +2,9 @@ let temperature = 0
 let humidity = 0
 let pressure = 0
 basic.forever(() => {
-    pressure = BME280.pressure()
+    pressure = BME280.pressure(BME280_P.Pa)
     humidity = BME280.humidity()
-    temperature = BME280.temperature()
+    temperature = BME280.temperature(BME280_T.T_C)
     serial.writeValue("P", pressure)
     serial.writeValue("H", humidity)
     serial.writeValue("T", temperature)
